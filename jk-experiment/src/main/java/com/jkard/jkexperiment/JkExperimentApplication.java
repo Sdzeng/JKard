a41df6c7f3c3186f75b8b4a3409ff01a923b3f23
dunshan_zeng <dunshan_zeng@aiagain.com>
2022-04-12 15:13:49 +0800

    great

---
+++ jk-experiment/src/main/java/com/jkard/jkexperiment/JkExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.jkard.jkexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JkExperimentApplication.class, args);
+    }
+
+}
---
+++ jk-experiment/src/test/java/com/jkard/jkexperiment/JkExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkard.jkexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
